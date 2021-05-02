39105d2d0d33b51ecdf7313adaab9d7e2dd1d262
islemsdiri <islemsdiri1989@gmail.com>
2021-05-03 01:52:53 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import React from "react"
+import MyProfilePicture from "./Component/Profile/ProfilPhoto"
+import MyName from "./Component/Profile/FullName"
+import MyAdress from "./Component/Profile/Address"
+
+
+import './App.css';
+
+const App=()=>(
+    
+    <>
+   
+   <MyProfilePicture/>
+    <MyName/>
+   <MyAdress/>
+   
+    </>
+  
+)
+
+
+
+export default App;
---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,10 @@
+import React from "react";
+const MyAdress=()=>{
+    return(
+    <div className="Body">
+        <h3>I leave in California:<br/>5454 Interstate 55 North Frontage Rd, Jackson, MS 39211, États-Unis</h3>
+    </div>)
+ 
+          
+}
+export default MyAdress;
